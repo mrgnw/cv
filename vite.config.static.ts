57519c3d695485c9bf8fd13849b709e6ea19c98e
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-09-30 22:20:44 +0200

    less moving parts

--- vite.config.static.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import svelteConfig from "./svelte.config.static.js";
-
-export default defineConfig({
-	plugins: [sveltekit(svelteConfig)],
-});
