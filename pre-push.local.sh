2c1cdb7b25206d81950789e75592c6ff69728ad9
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-12-09 17:35:25 +0100

    always generate pdfs on primary

--- pre-push.local.sh
+++ pre-push.local.sh
@@ -3,15 +3,5 @@
 echo "Generating PDFs..."
 bun run pdfs
 
-# If there are PDF changes, stash other changes, commit PDFs, and restore stash
-if [[ -n $(git status --porcelain static/*.pdf) ]]; then
-    # Stash any other pending changes
-    git stash push --keep-index
-
-    # Stage and commit PDFs
-    git add static/*.pdf
-    git commit -m "chore: update PDFs [skip ci]"
-    
-    # Restore any stashed changes
-    git stash pop || true
-fi
+git add static/*.pdf
+git commit -m "chore: update PDFs [skip ci]" || true
