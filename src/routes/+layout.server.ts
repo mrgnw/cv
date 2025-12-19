a34c222577f3d51d215645746041f8a9b083fc05
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-12-19 04:43:17 +0100

    clear!

--- src/routes/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import { getAllVersions } from '$lib/versionReader';
-
-export const load = (async () => {
-	return { versions: Object.values(getAllVersions()) };
-}) satisfies LayoutServerLoad;
--- use_template.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-word(){ sed `perl -e "print int rand(99999)"`"q;d" /usr/share/dict/words }
-
-appname=$1
-gh repo create $appname \
-  --template https://github.com/mrgnw/shadcn-svelte-template \
-  --private --clone
-cd $appname
